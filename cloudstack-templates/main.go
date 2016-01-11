ccb51cd52401b4319662aa386603e9ba3291dabc
Stefan Magnus Landrø <stefan.landro@gmail.com>
2016-01-11 12:48:43 +0100

    Added optional argument for keyword filtering of templates

--- cloudstack-templates/main.go
+++ cloudstack-templates/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/digipost/cloud-tools/config"
 	"github.com/xanzy/go-cloudstack/cloudstack"
 	"sort"
+	"os"
 )
 
 // Utility will return a list of templates sorted by date
+// Use optional argument to filter using keyword
 func main() {
+
+	var keyword string  = ""
+
+	if len(os.Args) == 2 {
+		keyword = os.Args[1]
+	}
 
 	apiurl, apikey, secret := config.CloudstackClientConfig()
 	client := cloudstack.NewClient(apiurl, apikey, secret, true)
 	templateService := cloudstack.NewTemplateService(client)
 
 	params := templateService.NewListTemplatesParams("community")
+	if keyword != "" {
+		params.SetKeyword(keyword)
+	}
 
 	if templates, err := templateService.ListTemplates(params); err != nil {
 		panic(err)
