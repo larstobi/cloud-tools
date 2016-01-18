95c8c5eecdbce02fcb6303256d06a6c7ded29b43
Stefan Magnus Landrø <stefan.landro@gmail.com>
2016-01-18 14:25:00 +0100

    Added date/time log statements

--- terraform-wrapper/main.go
+++ terraform-wrapper/main.go
@@ -4,17 +4,20 @@ import (
 	"github.com/digipost/cloud-tools/config"
 	"github.com/digipost/cloud-tools/wrapper"
 	"os"
+	"fmt"
+	"time"
 )
 
 // terraform-wrapper will get secrets from your pass password store,
 // setup an environment containing secrets and execute terraform,
 // passing command-line arguments to terraform as-is
 func main() {
-
+	fmt.Println("Starting terraform-wrapper at:", time.Now())
 	config := config.ParseDefaultCloudConfig()
 	secEnv := wrapper.GetEnvironmentVariablesForSecrets(config.SecretVariables[:])
 	env := wrapper.GetEnvironmentVariablesForValues(config.Variables[:])
 	wrapper.ExecuteTerraform("terraform", os.Args[1:], append(secEnv, env...))
+	fmt.Println("Ending terraform-wrapper at:", time.Now())
 
 }
 
