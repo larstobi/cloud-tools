fea2e92bc6f3446d5ace43ab0662b3f1b570dd0f
Stefan Magnus Landrø <stefan.landro@gmail.com>
2016-02-25 13:00:32 +0100

    Refactored test

--- vpn-profile-install/main_test.go
+++ vpn-profile-install/main_test.go
@@ -6,8 +6,9 @@ func TestHostnameIsResolveCorrectly(t *testing.T) {
 
 	hostname := getVpnHostnameForVpcName("sign_opstest")
 
-	if hostname != "vpn.opstest.signering.posten.no" {
-		t.Error("Expected vpn.opstest.signering.posten.no, got ", hostname)
+	expected := "vpn.opstest.signering.posten.no"
+	if hostname != expected {
+		t.Error("Expected ", expected, ", got ", hostname)
 	}
 
 }
